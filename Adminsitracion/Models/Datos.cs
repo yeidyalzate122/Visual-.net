047f446ca5f2098575ff3007490d6fcaae3b50f8
yei <yalzate>
2018-09-03 15:23:00 -0500

    Proyect

---
+++ Adminsitracion/Models/Datos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Adminsitracion.Models
+{
+    public class Datos
+    {
+        [Required(ErrorMessage = "el siguiente dato es el que requiero")]
+        public String nombre { get; set; }
+
+        [Required(ErrorMessage = "el siguiente dato es el que requiero")]
+        public String apellido { get; set; }
+
+        [Required(ErrorMessage = "el siguiente dato es el que requiero")]
+        public String edad { get; set; }
+
+
+    }
+}
