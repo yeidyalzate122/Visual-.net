edf0519fa3968e567f59b4a7c61abf68ec2b29f6
yei <yalzate>
2018-09-03 11:43:41 -0500

    Proyecto sencillo

---
+++ Adminsitracion/Adminsitracion/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Adminsitracion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Adminsitracion.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public ActionResult DatosPersonales(Datos obj )
+        {
+
+            // se valida si los datos estan
+            if(ModelState.IsValid)
+               {
+
+                return View(obj);
+
+            }
+            else
+            {
+                return View("Index");
+            }
+            
+        }
+    }
+}
